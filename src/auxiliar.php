eeef25328917dd8b2f0a8e6ca6cf03868daf2181
agucadiz <agustin.pedrote@iesdonana.org>
2022-12-03 21:02:40 +0100

    index.php y función hh

--- src/auxiliar.php
+++ src/auxiliar.php
@@ -5,3 +5,7 @@ function conectar()
     return new \PDO('pgsql:host=localhost,dbname=alumnos', 'alumnos', 'alumnos');
 }
 
+function hh($x)
+{
+    return htmlspecialchars($x ?? '', ENT_QUOTES | ENT_SUBSTITUTE);
+}
