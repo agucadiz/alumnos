9e9c0f30636bbbd6fc959a44c8352d46ab427e8c
agucadiz <agustin.pedrote@iesdonana.org>
2022-12-04 14:18:32 +0100

    Modificar y CRUD funcionan

--- public/index.php
+++ public/index.php
@@ -31,11 +31,16 @@
                         <td class="py-4 px-6"><?= hh($fila['nombre']) ?></td>
 
                         <td class="py-4 px-6 text-center">
-                            <a href="modificar.php?id=<?= $fila['id'] ?>&nombrem=<?= $fila['nombre'] ?>" class="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900">Editar</a>
+                            <a href="modificar.php?id=<?= hh($fila['id']) ?>" class="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900">
+                                Editar</a>  
                             <form action="borrar.php" method="POST" class="inline">
                                 <input type="hidden" name="id" value="<?= hh($fila['id']) ?>">
-                                <button type="submit" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Borrar</button>
+                                <button type="submit" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                                    Borrar
+                                </button>
                             </form>
+                            <!-- <a href="borrar.php?id=<?= hh($fila['id']) ?>" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                                Borrar</a> -->
                         </td>
                     </tr>
                 <?php endforeach ?>
@@ -43,7 +48,10 @@
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                     <td></td>
                     <td class="py-4 px-6 text-center">
-                        <a href="insertar.php" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Insertar alumno</a>
+                        <a href="insertar.php" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                            Insertar alumno
+                        </a>
+                    </td>
                 </tr>
             </tbody>
         </table>
