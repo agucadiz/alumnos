fdb9cd4feffa49066880ca9591a38ea0459830b5
agucadiz <agustin.pedrote@iesdonana.org>
2022-12-07 12:57:25 +0100

    Arreglado el borrado de alumnos

--- public/admin/borrar.php
+++ public/admin/borrar.php
@@ -13,8 +13,21 @@ if (!isset($id)) {
 }
 
 // TODO. Comprobar si el alumno tiene notas.
-
 $pdo = conectar();
-Alumno::borrar($id);
-$_SESSION['exito'] = 'El alumno se ha borrado correctamente.';
-volver_admin();
+$pdo->beginTransaction();
+$pdo->exec('LOCK TABLE notas IN SHARE MODE');
+$sent = $pdo->prepare("SELECT COUNT(*) 
+                       FROM alumnos 
+                       JOIN notas 
+                       ON alumnos.id=notas.alumno_id
+                       WHERE alumnos.id = :id;");
+$sent->execute([':id' => $id]);
+if ($sent->fetchColumn() === 0) {
+    Alumno::borrar($id);
+    $_SESSION['exito'] = 'El alumno se ha borrado correctamente.';
+    volver_admin();
+} else {
+    $_SESSION['error'] = 'El alumno tiene notas asociadas.';
+    volver_admin();
+}
+
